ed574f18523742162c6991bfd29c4f66ced0056b
chococooke * web3 <pro.bhimsen@gmail.com>
2025-04-30 13:43:17 +0530

    add axios

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 
 const app = express();
 
+let products = [];
+
 app.use(express.json());
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/", (req, res) => {
+    const product = req.body;
+    products.push(product);
+
+    res.send("Added product successfully");
 })
 
 app.listen(5000, () => {
